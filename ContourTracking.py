b617533481daa4d581616f919b6f2fa161d47475
OrestChura <orest-chura@yandex.ru>
2019-04-05 22:29:20 +0300

    Промеж.

--- ContourTracking.py
+++ ContourTracking.py
@@ -10,12 +10,11 @@ picts = []
 conts = []
 
 for i in [1, 2]:
-
-    pfluor = cv.imread('Test\\'+name + str(i) + '_' + str(wl) + '.tiff', cv.IMREAD_GRAYSCALE)
-    p0 = cv.imread('Test\\'+name+str(i)+'_0.tiff', cv.IMREAD_GRAYSCALE)
-    p = pfluor-p0
+    pfluor = cv.imread('Test\\' + name + str(i) + '_' + str(wl) + '.tiff', cv.IMREAD_GRAYSCALE)
+    p0 = cv.imread('Test\\' + name + str(i) + '_0.tiff', cv.IMREAD_GRAYSCALE)
+    p = pfluor - p0
     ma1 = np.max(p)
-    pshow = (p*(255/ma1)).astype(np.uint8)
+    pshow = (p * (255 / ma1)).astype(np.uint8)
     cv.namedWindow('picture', cv.WINDOW_NORMAL)
     cv.imshow('picture', pshow)
     cv.waitKey(0)
@@ -29,6 +28,37 @@ for i in [1, 2]:
     # cv.imshow('thresh', thsimple)
     # cv.waitKey(0)
 
+    # TODO: найти оптимальный Гаусс
+    # import cv2 as cv
+    # import numpy as np
+    # import matplotlib.pyplot as plt
+    #
+    # name = 'T'
+    # # i = 1
+    # wl = 400
+    #
+    # picts = []
+    # conts = []
+    #
+    # i = 1
+    #
+    # pfluor = cv.imread('Test\\' + name + str(i) + '_' + str(wl) + '.tiff', cv.IMREAD_GRAYSCALE)
+    # p0 = cv.imread('Test\\' + name + str(i) + '_0.tiff', cv.IMREAD_GRAYSCALE)
+    # p = pfluor - p0
+    # ma1 = np.max(p)
+    # pshow = (p * (255 / ma1)).astype(np.uint8)
+    # cv.namedWindow('picture', cv.WINDOW_NORMAL)
+    # cv.imshow('picture', pshow)
+    # cv.waitKey(0)
+    #
+    # cv.namedWindow('picture1', cv.WINDOW_NORMAL)
+    # for i in (range(10)):
+    #     blur = cv.GaussianBlur(p, (i * 2 + 1, i * 2 + 1), 0)
+    #     blurshow = (blur * (255 / ma1)).astype(np.uint8)
+    #
+    #     cv.imshow('picture1', blurshow)
+    #     cv.waitKey(0)
+
     blur = cv.GaussianBlur(p, (9, 9), 0)
     blurshow = (blur * (255 / ma1)).astype(np.uint8)
     cv.namedWindow('picture1', cv.WINDOW_NORMAL)
@@ -37,7 +67,7 @@ for i in [1, 2]:
     # plt.hist(blur.ravel(), 256)
     # plt.show()
 
-    m, thgauss = cv.threshold(blur, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    m, thgauss = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
     print(m)
     cv.namedWindow('threshblur', cv.WINDOW_NORMAL)
     cv.imshow('threshblur', thgauss)
@@ -53,6 +83,7 @@ for i in [1, 2]:
     cv.imshow('contoured', cv.drawContours(cv.cvtColor(thgauss, cv.COLOR_GRAY2RGB), cont, -1, (0, 255, 0), 2))
     cv.waitKey(0)
 
+# TODO: возможно, исп. эту ф-ю
 # cv.goodFeaturesToTrack() - функция для отыскания углов
 
 nextPts, status, err = cv.calcOpticalFlowPyrLK(picts[0], picts[1],
