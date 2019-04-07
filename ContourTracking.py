69aa91a4d453c7a92317492c466be7ee0d936551
OrestChura <orest-chura@yandex.ru>
2019-04-07 22:44:31 +0300

    squash! Промеж.

--- ContourTracking.py
+++ ContourTracking.py
@@ -9,7 +9,7 @@ wl = 400
 picts = []
 conts = []
 
-for i in [1, 2]:
+for i in range(1, 20):
     pfluor = cv.imread('Test\\' + name + str(i) + '_' + str(wl) + '.tiff', cv.IMREAD_GRAYSCALE)
     p0 = cv.imread('Test\\' + name + str(i) + '_0.tiff', cv.IMREAD_GRAYSCALE)
     p = pfluor - p0
@@ -59,19 +59,19 @@ for i in [1, 2]:
     #     cv.imshow('picture1', blurshow)
     #     cv.waitKey(0)
 
-    blur = cv.GaussianBlur(p, (9, 9), 0)
+    blur = cv.GaussianBlur(p, (5, 5), 0)
     blurshow = (blur * (255 / ma1)).astype(np.uint8)
-    cv.namedWindow('picture1', cv.WINDOW_NORMAL)
-    cv.imshow('picture1', blurshow)
-    cv.waitKey(0)
+    # cv.namedWindow('picture1', cv.WINDOW_NORMAL)
+    # cv.imshow('picture1', blurshow)
+    # cv.waitKey(0)
     # plt.hist(blur.ravel(), 256)
     # plt.show()
 
     m, thgauss = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-    print(m)
-    cv.namedWindow('threshblur', cv.WINDOW_NORMAL)
-    cv.imshow('threshblur', thgauss)
-    cv.waitKey(0)
+    # print(m)
+    # cv.namedWindow('threshblur', cv.WINDOW_NORMAL)
+    # cv.imshow('threshblur', thgauss)
+    # cv.waitKey(0)
 
     picts.append(thgauss)
 
@@ -79,16 +79,16 @@ for i in [1, 2]:
 
     conts = conts + cont
 
-    cv.namedWindow('contoured', cv.WINDOW_NORMAL)
-    cv.imshow('contoured', cv.drawContours(cv.cvtColor(thgauss, cv.COLOR_GRAY2RGB), cont, -1, (0, 255, 0), 2))
-    cv.waitKey(0)
+    # cv.namedWindow('contoured', cv.WINDOW_NORMAL)
+    # cv.imshow('contoured', cv.drawContours(cv.cvtColor(thgauss, cv.COLOR_GRAY2RGB), cont, -1, (0, 255, 0), 3))
+    # cv.waitKey(0)
 
 # TODO: возможно, исп. эту ф-ю
 # cv.goodFeaturesToTrack() - функция для отыскания углов
 
-nextPts, status, err = cv.calcOpticalFlowPyrLK(picts[0], picts[1],
-                                               np.float32([tr[-1] for tr in conts[0]]).reshape(-1, 1, 2), None)
-cv.namedWindow('contoured_by', cv.WINDOW_NORMAL)
-cv.imshow('contoured_by', cv.drawContours(cv.cvtColor(thgauss, cv.COLOR_GRAY2RGB),
-                                          [np.int32(np.around(nextPts))], -1, (0, 0, 255), 2))
-cv.waitKey(0)
+# nextPts, status, err = cv.calcOpticalFlowPyrLK(picts[0], picts[1],
+#                                                np.float32([tr[-1] for tr in conts[0]]).reshape(-1, 1, 2), None)
+# cv.namedWindow('contoured_by', cv.WINDOW_NORMAL)
+# cv.imshow('contoured_by', cv.drawContours(cv.cvtColor(thgauss, cv.COLOR_GRAY2RGB),
+#                                           [np.int32(np.around(nextPts))], -1, (0, 0, 255), 3))
+# cv.waitKey(0)
